3ab98be8570f534a7442555d0c2b388ef10d6636
Martine Middelthon <m.middelthon@hotmail.com>
2020-09-10 14:19:23 +0200

    excel fil

--- Exercise_3.R
+++ Exercise_3.R
@@ -8,3 +8,9 @@ df$Dato <- as.Date(df$Dato, "%d.%m.%Y")
 plot(df$Dato, df$`Kumulativt antall`, type="o", xlab="Date", ylab= "Cumulative cases")
 plot(df$Dato, df$`Nye tilfeller`, type="o", xlab="Date", ylab= "New cases")
 
+
+#PACF page 86
+x = df$`Nye tilfeller`
+pacf(x, lag = length(x), pl= TRUE)
+#y = df$`Kumulativt antall`
+#pacf(y, lag = length(y), pl = TRUE)
