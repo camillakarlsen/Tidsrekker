db90f5dfe1e096d854068cbbb9a08ec3ed8601dc
Martine Middelthon <m.middelthon@hotmail.com>
2020-09-12 15:34:46 +0200

    begynt bittelitt

--- Exercise_3.R
+++ Exercise_3.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(astsa)
+library(itsmr)
 
 df <- read_xlsx("xls.xlsx",skip=1)
 df$Dato <- as.Date(df$Dato, "%d.%m.%Y") 
@@ -10,7 +11,13 @@ plot(df$Dato, df$`Nye tilfeller`, type="o", xlab="Date", ylab= "New cases")
 
 
 #PACF page 86
-x = df$`Nye tilfeller`
-pacf(x, lag = length(x), pl= TRUE)
-#y = df$`Kumulativt antall`
-#pacf(y, lag = length(y), pl = TRUE)
+#x = df$`Nye tilfeller`
+#pacf(x, lag = length(x)-1, pl= TRUE)
+# skal vi bruke kumulativt antall eller nye tilfeller?
+y = df$`Kumulativt antall`
+pacf(y, lag = length(y)-1, pl = TRUE) #litt rart at kun pacf ved lag 0 er utenfor de blå strekene?
+mu = mean(y) # sample mean
+mu
+autocov = acvf(y, length(y)-1)
+gamma0 = autocov[1] # acvf at lag 0
+gamma1 = autocov[2]
