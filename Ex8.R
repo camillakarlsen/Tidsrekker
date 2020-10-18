bd6fb86da0530d98b8121dbc19a7faf190229e02
Martine Middelthon <m.middelthon@hotmail.com>
2020-10-18 16:43:15 +0200

    filer

---
+++ Ex8.R
@@ -0,0 +1,22 @@
+#Loading packages 
+library("readxl")
+library("forecast")
+library("tseries")
+
+#Reading data from excel
+df <- read_xlsx("C:\\Users\\marti\\Documents\\NTNU\\Tidsrekker\\Tidsrekker\\xls_ex8.xlsx",skip=1)
+df$Dato <- as.Date(df$Dato, "%d.%m.%Y") 
+
+#Plot the data
+plot(df$Dato, df$`Kumulativt antall`, type="l", xlab="Date", 
+     ylab= "Cumulative cases")
+plot(df$Dato, df$`Nye tilfeller`, type="o", xlab="Date", ylab= "New cases")
+
+## Create a time series object
+dayofYear <- (df$Dato[1] - as.Date("2020-01-01") + 1)
+tseries <- ts(df$`Nye tilfeller`,
+              start = c(2020, dayofYear),
+              frequency = 365)
+
+plot.ts(tseries, ylab="New cases", type="o")
+summary(tseries)
