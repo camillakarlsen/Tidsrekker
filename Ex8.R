e9e884575b75c1a6b1642950e3c8ca4fe9be0c2b
Camilla <camikar@stud.ntnu.no>
2020-10-21 20:58:01 +0200

    Manuell Arimafit

--- Ex8.R
+++ Ex8.R
@@ -6,7 +6,7 @@ library("astsa")
 
 #Reading data from excel
 #setwd("/Users/camillakarlsen/Desktop/Tidsrekker/Tidsrekker")
-df <- read_xlsx("xls_ex8.xlsx",skip=1) #[-c(236),] Fjerne siste rad siden denne ikke er fullstendig?
+df <- read_xlsx("xls_ex8.xlsx",skip=1)# [-c(236),] #Fjerne siste rad siden denne ikke er riktig?
 #df <- read_xlsx("C:\\Users\\marti\\Documents\\NTNU\\Tidsrekker\\Tidsrekker\\xls_ex8.xlsx",skip=1)
 df$Dato <- as.Date(df$Dato, "%d.%m.%Y") 
 
@@ -57,29 +57,47 @@ transformed.data = diff(transformed, lag = 7, differences = 1)
 plot.ts(transformed.data,type="l")
 abline(h=mean(transformed.data), col="red")
 
-acf(transformed.data, main="")
-pacf(transformed.data, main="")
+acf(transformed.data, main="") #q = 1, Q = 1 eller 2 
+pacf(transformed.data, main="") #p = 1, P = 0 tails off
 
-#ARIMA
-fit1 <- auto.arima(transformed.data, ic="aicc", trace=TRUE, d=1)
-fit1
-checkresiduals(fit1$residuals, test="FALSE")
-Box.test(fit1$residuals, type = "Ljung-Box", lag=10, fitdf = 2)
+model_from_acf = Arima(boxcox_fit, order=c(1,1,1), seasonal = list(order=c(0,1,2),period=7),method="ML")
+model_from_acf
+#Fit ARIMA model - AICC
+lowest_aicc <- 10000
+for (p in 0:2){
+  for (q in 0:2){
+    for (P in 0:2){
+      for (Q in 0:2){
+        model = Arima(boxcox_fit, order=c(p,1,q), seasonal = list(order=c(P,1,Q),period=7),method="ML")
+        AICC = model$aicc
+        if (AICC<lowest_aicc){
+          best_model <- model
+          lowest_aicc <- AICC
+        }
+      }
+    }
+  }
+}
 
-#SARIMA
-sarima = astsa::sarima(boxcox_fit, 3, 1, 2, 0, 1, 2, 7)
-sarima$fit
+best_model #ARIMA(1,1,0)(0,1,2)[7]
 
-#Forecast next 14 days 
-forecast <- forecast::forecast(sarima$fit, h=14, biasadj=TRUE)
+
+#Check residuals
+checkresiduals(best_model$residuals, test="FALSE")
+Box.test(best_model$residuals, type = "Ljung-Box", lag=20, fitdf = 3) #hva skal lag være? 
+
+
+#Forecast next 14 days (blir veldig lav? på grunn av siste registrering?)
+forecast <- forecast::forecast(best_model, h=14, biasadj=TRUE)
 forecast$mean <- InvBoxCox(forecast$mean,lambda=lambda)
 forecast$upper <- InvBoxCox(forecast$upper, lambda = lambda)
 forecast$lower <- InvBoxCox(forecast$lower, lambda = lambda)
-forecast$upper
 forecast$x <- tseries
 autoplot(forecast)
-forecast$mean
 
-#SARIMA forecast
-sarima.for(boxcox_fit, 14, 3,1,2,0,1,2,7) #ikke transformert tilbake
-?sarima.for
+#Simulating
+?arima.sim
+
+#GARCH
+
+
